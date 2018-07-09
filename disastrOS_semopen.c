ebad714b1d05a83fa7b60ee0f549c5d93db27d5f
Ionut <ionut.ticu.94@gmail.com>
2018-07-09 11:58:02 +0200

    Iniziata sem_open

--- disastrOS_semopen.c
+++ disastrOS_semopen.c
@@ -7,8 +7,28 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semOpen(){
-	//Mi "ispiro" all'open_resource fornita
-	//Prendo gli argomenti dal processo che chiamerà sem_open
+	/*Mi "ispiro" all'open_resource fornita
+		Prendo gli argomenti dal processo che chiamerà sem_open
+		Resource_id mi identifica l'id della risorsa su cui voglio operare
+		il count il contatore del semaforo.
+	*/
 	int resource_id = running->syscall_args[0];
 	int count = running->syscall_args[1];
+	
+	//Vediamo se per caso questo semaforo non esiste già...
+	Semaphore* sem = SemaphoreList_byId(&semaphores_list,id);
+	
+	//L'ho trovato?Esiste?Boh
+	
+	if(!sem) {
+		//Alloco
+		sem = Semaphore_alloc(id,count);
+		//Inserisco nella lista dei sem
+		List_insert(&semaphores_list,semaphores_list.last,(ListItem*) sem);
+	}
+	//Problemi nel frattempo??
+	if(!sem) {
+		running->syscall_retvalue = DSOS_ESEMAPHOREPROBLEM;
+		return;
+	}
 }
