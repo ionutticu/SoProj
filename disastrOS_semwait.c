4252e5021374035499b923ef5999fe764b466489
Ionut <ionut.ticu.94@gmail.com>
2018-07-09 18:06:32 +0200

    Sem_close implementata,più aggiunte intestazioni operazioni semafori

--- disastrOS_semwait.c
+++ disastrOS_semwait.c
@@ -7,5 +7,47 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semWait(){
-  // do stuff :)
+
+	int fd = running->syscall_args[0];
+	
+	SemDescriptor* descr = SemDescriptorList_byFd(&running->sem_descriptors,fd);
+	
+	if(!descr){
+		running->syscall_retvalue = DSOS_ESEMAPHORENODESC;
+		return;
+	}
+
+	SemDescriptorPtr* descrptr = descr->ptr;
+
+	if(!descrptr){
+		running->syscall_retvalue = DSOS_ESEMAPHORENODESCPTR;
+		return;
+	}
+
+	Semaphore* sem = descr->semaphore;
+
+	if(!sem){
+		running->syscall_retvalue = DSOS_ESEMAPHOREPROBLEM;
+	}
+	
+	sem->count = (sem->count -1);
+
+	PCB* processo;
+
+	if(sem->count < 0){
+		//Boh, funzionerà cosi?
+		List_detach(&sem->descriptors,(ListItem*) descrptr);
+		List_insert(&sem->waiting_descriptors,sem->waiting_descriptors.last,(ListItem*) descr->ptr);
+
+		//Switch processi
+		List_insert(&waiting_list,waiting_list.last,(ListItem*) running);
+		processo = (PCB*) List_detach(&ready_list,(ListItem*) ready_list.first);
+		running = (PCB*) processo;
+	
+	}
+	
+	running->syscall_retvalue = 0;
+
+	return;
+		
 }
