2d7922df6c5d1eb761fa9fe326c9b3af024053e2
Ionut <ionut.ticu.94@gmail.com>
2018-07-09 17:44:00 +0200

    Semclose fatta

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -7,5 +7,35 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semClose(){
-  // do stuff :)
+	//Prendo parametro dalla syscall,il fd in questo caso
+	int fd = running->syscall_args[0];
+
+	SemDescriptor* descr = SemDescriptorList_byFd(&running->sem_descriptors,fd);
+
+	//Qualcosa storto?
+	
+	if(!descr){
+		running->syscall_retvalue = DSOS_ESEMAPHOREPROBLEM;
+		return;
+	}
+
+	descr = (SemDescriptor*) List_detach(&running->descriptors,(ListItem*) descr);
+	
+	assert(descr);
+
+	//Bene qui viene il bello
+
+	Semaphore* sem = descr->semaphore;
+
+	SemDescriptorPtr* descrptr = (SemDescriptorPtr*) List_detach(& sem->descriptors,(ListItem*)descr->ptr);
+
+	assert(descrptr);
+
+	SemDescriptor_free(descr);
+	SemDescriptorPtr_free(descrptr);
+	
+	//Tutto ok,posso tornare
+
+	running->syscall_retvalue = 0;
+  
 }
