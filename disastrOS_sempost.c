3faa2766caf39a7bb9ec92c86405f321bd1cae3b
Ionut <ionut.ticu.94@gmail.com>
2018-07-09 19:12:52 +0200

    Sem_post finita.Mancano i test

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -7,5 +7,61 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semPost(){
-  // do stuff :)
+
+	int fd = running->syscall_args[0];
+
+	SemDescriptor* descr = SemDescriptorList_byFd(&running->sem_descriptors,fd);
+	
+	//Soliti tre controlli a cascata
+	if(!descr){
+		running->syscall_retvalue = DSOS_ESEMAPHORENODESC;
+		return;
+	}
+	
+	SemDescriptorPtr* descrptr = descr->ptr;
+
+	if(!descrptr){
+		running->syscall_retvalue = DSOS_ESEMAPHORENODESCPTR;
+		return;	
+	}
+
+	Semaphore* sem = descr->semaphore;
+	
+	if(!sem){
+		running->syscall_retvalue = DSOS_ESEMAPHOREPROBLEM;
+		return;
+	}
+
+
+	//Incremento semafor
+	sem->count = (sem->count+1);
+
+	if(sem->count >= 0){
+
+		List_detach(&sem->waiting_descriptors,(ListItem*) descr);
+		List_insert(&sem->descriptors,sem->descriptors.last,(ListItem*)descr);	
+
+		//Tolgo dalla waiting list il processo bloccato dal sem,descrptr->pcb
+		//Lo inserisco in ready list
+		
+
+		List_detach(&waiting_list,(ListItem*) descr->pcb);
+		List_insert(&ready_list,ready_list.last,(ListItem*) descr->pcb);
+	}
+	
+	running->syscall_retvalue = 0;
+	return;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
